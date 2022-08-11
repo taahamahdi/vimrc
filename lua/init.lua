4d250b9a6c5d4fd6045dfc744171af7247d94150
taahamahdi <mahditaaha11@gmail.com>
2022-08-11 19:03:54 -0400

    Remove markdownlint

--- lua/init.lua
+++ lua/init.lua
@@ -330,7 +330,6 @@ local sources = {
     null_ls.builtins.code_actions.proselint,
     null_ls.builtins.code_actions.eslint_d,
     null_ls.builtins.diagnostics.write_good,
-    null_ls.builtins.diagnostics.markdownlint,
     null_ls.builtins.diagnostics.misspell,
     null_ls.builtins.formatting.prettier,
     null_ls.builtins.diagnostics.alex,
